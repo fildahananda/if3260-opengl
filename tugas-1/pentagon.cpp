a5665f2af2017aa3bca3d31b98c72f53cbd9ca1c
Richard Matthew <richard.matthew20@gmail.com>
2019-04-09 21:14:37 +0700

    update tugas 1

--- tugas-1/pentagon.cpp
+++ tugas-1/pentagon.cpp
@@ -43,15 +43,15 @@ void display() {
 }
  
 /* Main function: GLUT runs as a console application starting at main()  */
-//int main(int argc, char** argv) {
-//   glutInit(&argc, argv);                 // Initialize GLUT
-//   glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
-//   glutInitWindowSize(320, 320);   // Set the window's initial width & height
-//   glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
-//   glutDisplayFunc(display); // Register display callback handler for window re-paint
-//   glutMainLoop();           // Enter the infinitely event-processing loop
-//   return 0;
-//}
+int main(int argc, char** argv) {
+   glutInit(&argc, argv);                 // Initialize GLUT
+   glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
+   glutInitWindowSize(320, 320);   // Set the window's initial width & height
+   glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+   glutDisplayFunc(display); // Register display callback handler for window re-paint
+   glutMainLoop();           // Enter the infinitely event-processing loop
+   return 0;
+}
 
 /* compile & run
 g++ -O0 -g3 -Wall -c -fmessage-length=0 -o pentagon.o pentagon.cpp 
